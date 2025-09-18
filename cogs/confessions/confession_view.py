dffdeb5ae396bede471d5f48f9b875295d128590
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2025-09-18 15:13:36 +0200

    Fix import error

--- cogs/confessions/confession_view.py
+++ cogs/confessions/confession_view.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ui import Button, View
-from .rules_modal import Rulesmodal
+from .rules_modal import RulesModal
 
 
 class ConfessionView(View):
@@ -10,4 +10,4 @@ class ConfessionView(View):
 
     @discord.ui.button(label="Submit Confession", style=discord.ButtonStyle.primary, custom_id="submit_confession")
     async def submit_confession(self, interaction: discord.Interaction, button: Button):
-        await interaction.response.send_modal(Rulesmodal(self.bot))
+        await interaction.response.send_modal(RulesModal(self.bot))
